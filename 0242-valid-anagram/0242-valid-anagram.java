36ef72b3725b36de1898f8dd3d069dc93afdbdf3
Vinayak Sannaik <112576218+Vinayak-Sannaik@users.noreply.github.com>
2025-09-17 06:46:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> sMap = new HashMap<>();
+        Map<Character, Integer> tMap = new HashMap<>();
+
+        for(char c: s.toCharArray()){
+            sMap.put(c, sMap.getOrDefault(c, 0)+1);   // s = "a:2"
+        }
+
+        for(char c: t.toCharArray()){
+            tMap.put(c, tMap.getOrDefault(c, 0)+1);   // t = "a:1"
+        }
+
+        System.out.print(sMap.keySet() + tMap.keySet());
+
+        // return sMap.keySet().equals(tMap.keySet()) && sMap.values().equals(tMap.values());
+        // return new HashSet<>(sMap.values()).equals(new HashSet<>(tMap.values()));
+        return sMap.equals(tMap);
+    }
+}
