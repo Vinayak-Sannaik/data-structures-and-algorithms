5871f5f243f170393fa09f8e08ff7d621dd0f648
Vinayak Sannaik <112576218+Vinayak-Sannaik@users.noreply.github.com>
2026-02-04 22:55:06 +0530

    Time: 1 ms (71.97%), Space: 45 MB (10.46%) - LeetHub

--- 3637-trionic-array-i/3637-trionic-array-i.java
+++ 3637-trionic-array-i/3637-trionic-array-i.java
@@ -1,51 +1,100 @@
 // class Solution {
 //     public boolean isTrionic(int[] nums) {
-//         if(nums.length < 6){
+//         if (nums.length < 4) {
 //             return false;
 //         }
-//         int conditionsMet = 0;
-//         for(int i = 0; i< nums.length - 1; i++){
-//             if(nums[i] > nums[i+1]){
-//                 conditionsMet++;
-//             }else{
-//                 conditionsMet++;
-//             }    
 
+//         int n = nums.length;
+//         int i = 0;
+//         while (i < n - 1 && nums[i] > nums[i + 1]) {
+//             i++;
 //         }
-        
-//         return conditionsMet == 3;
+//         // if we are at end and i not incremented we not found increase
+//         if (i == 0 || i == n - 1)
+//             return false;
+
+//         int p = i;
+//         while (i < n - 1 && nums[i] < nums[i + 1]) {
+//             i++;
+//         }
+//         if (p == i || i == n - 1)
+//             return false;
+
+//         while (i < n - 1 && nums[i] > nums[i + 1]) {
+//             i++;
+//         }
+
+//         if (i == n - 1) {
+//             return true;
+//         } else {
+//             return false;
+//         }
+
 //     }
 // }
 
 class Solution {
     public boolean isTrionic(int[] nums) {
-        int n = nums.length;
-        if (n < 4) return false;
+        if (nums.length < 4) return false;
 
+        int n = nums.length;
         int i = 0;
 
-        while (i + 1 < n && nums[i] < nums[i+1]) {
+        // 1️⃣ Increasing
+        int start = i;
+        while (i < n - 1 && nums[i] < nums[i + 1]) {
             i++;
         }
+        if (i == start) return false;
 
-        if (i == 0 || i == n - 1) return false;
-
-        int p = i;
-        while (i + 1 < n && nums[i] > nums[i+1]) {
+        // 2️⃣ Decreasing
+        start = i;
+        while (i < n - 1 && nums[i] > nums[i + 1]) {
             i++;
         }
+        if (i == start) return false;
 
-        if (i == p || i == n - 1) return false;
-
-        while (i + 1 < n && nums[i] < nums[i+1]) {
+        // 3️⃣ Increasing
+        start = i;
+        while (i < n - 1 && nums[i] < nums[i + 1]) {
             i++;
         }
+        if (i == start) return false;
 
         return i == n - 1;
     }
 }
 
 
-// check nums>= 6
-// [1,3,5,4,2,6]
-//    ||   
+
+// class Solution {
+//     public boolean isTrionic(int[] nums) {
+//         int n = nums.length;
+//         if (n < 4) return false;
+
+//         int i = 0;
+
+//         while (i + 1 < n && nums[i] < nums[i+1]) {
+//             i++;
+//         }
+
+//         if (i == 0 || i == n - 1) return false;
+
+//         int p = i;
+//         while (i + 1 < n && nums[i] > nums[i+1]) {
+//             i++;
+//         }
+
+//         if (i == p || i == n - 1) return false;
+
+//         while (i + 1 < n && nums[i] < nums[i+1]) {
+//             i++;
+//         }
+
+//         return i == n - 1;
+//     }
+// }
+
+// // check nums>= 6
+// // [1,3,5,4,2,6]
+// //    ||   
